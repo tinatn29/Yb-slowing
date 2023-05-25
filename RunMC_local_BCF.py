8d0049e2b9b7646a968197d44b032f233388c058
tinatn29 <tanaporn.nanarong@gmail.com>
2023-05-25 10:52:26 -0700

    edit input settings

--- RunMC_local_BCF.py
+++ RunMC_local_BCF.py
@@ -20,6 +20,11 @@ loading_time = 1e-3 # loading time of 1ms
 
 MC = MCSolver(delta=delta, omega=omega, phi=-np.pi/4, loading_time=loading_time)
 
+# load v_initial from file to start with the same distribution
+# 2mm oven radius
+t_input = np.load('./input_files/t_old_new_input_2mm.npy')
+print(len(t_input))
+
 ## Uncomment to select chirp or fixed detuning
 '''
 # Test fixed detuning
@@ -27,15 +32,18 @@ MC.args['detuning'] = MC.args['k'] * v_detuning # velocity detuning
 print(f"Fixed detuning at {v_detuning:.1f} m/s, applied for {loading_time*1000:.1f} ms")
 '''
 
+'''
 # Test chirp
 chirp_period = 1e-3 # 1ms
 vmax = 10
 vmin = 0
 MC.SetChirping(chirp_period, vmax, vmin)
 print("Chirping between {0} m/s and {1} m/s at period of {2:.2f} ms".format(vmax, vmin, chirp_period * 1000))
+'''
 
 v_initial = np.array([0.0, 0.0, 10.0])
 r_initial = np.array([0.0, 0.0, 0.0])
 t_initial = 0
 output = MC.SolveMC_single_GaussianBeam_InputAll(v_initial, r_initial, t_initial)
 print(output)
+
